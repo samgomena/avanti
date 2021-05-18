543e74ac37eb9b8b0d2098e17f46105791f89632
sgomena <sgomena@tripwire.com>
2021-05-17 23:24:58 -0700

    feat: Add page info to home

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,55 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <section
+        class="section section_welcome"
+        data-parallax="scroll"
+        data-image-src="assets/img/1.jpg"
+      >
+        <div class="section_welcome__main">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col">
+                <div
+                  class="alert alert-warning alert-dismissible fade show text-center section__subheading"
+                  role="alert"
+                >
+                  We are open for table service from 4 - 9 PM!
+                  <hr />
+                  For to-go orders please see our
+                  <a href="menu.html" class="alert-link">
+                    to go menu
+                  </a>
+                  . Or,
+                  <a class="alert-link" href="tel:5038265631">
+                    call us!
+                  </a>
+                  <button
+                    type="button"
+                    class="close"
+                    data-dismiss="alert"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+
+                <h1 class="section__heading section_welcome__heading text-center">
+                  Avanti
+                  <br />
+                </h1>
+                <h2 class="section__preheading section_welcome__preheading text-center text-muted">
+                  <span class="text-primary">Restaurant and Bar</span>
+                </h2>
+                <p class="section__subheading section_welcome__subheading text-center text-muted">
+                  Located in Tualatin, Oregon.
+                </p>
+                <br />
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <section>
         <div class="section_welcome__footer">
