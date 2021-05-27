1e7b6ca13ab20f6faed6279a2c3b51393fa3d3d2
samgomena <samgomena@protonmail.com>
2021-05-26 23:01:33 -0700

    feat: Update pages to be ts compliant

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -5,13 +5,15 @@ import Layout from "../components/layout";
 export default function Menu() {
   return (
     <Layout>
-      <h1>Menu</h1>
-      <Image
-        src="/images/porkchop_in_window.jpg"
-        height={144}
-        width={144}
-        alt="Porkchop in Window"
-      />
+      <>
+        <h1>Menu</h1>
+        <Image
+          src="/images/porkchop_in_window.jpg"
+          height={144}
+          width={144}
+          alt="Porkchop in Window"
+        />
+      </>
     </Layout>
   );
 }
