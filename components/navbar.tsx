4196eed590ad79ec7dd147afa4ac844df666b026
samgomena <samgomena@protonmail.com>
2021-07-20 19:08:30 -0700

    feat: Upgrade navbar to use react-bootstrap b/c using native bootstrap events was horrendous

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,22 +1,15 @@
-import { Collapse } from "bootstrap";
 import Link from "next/link";
-import { useCallback, useEffect, useRef, useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
-export default function Navbar() {
+export default function AvantiNavbar() {
   const router = useRouter();
-  const getNavClass = useCallback(
-    (path: string) => `nav-link ${router.asPath === path ? "active" : ""}`,
-    [router]
-  );
-
   const [navbarTheme, setNavbarTheme] = useState<"dark" | "light">("dark");
-  const [showCollapse, setShowCollapse] = useState(false);
-  const navbarCollapse = useRef<HTMLDivElement>(null);
 
   const handleScroll = useCallback(() => {
-    const scrollTop = window.pageYOffset;
-    if (scrollTop <= 1) {
+    if (window.pageYOffset <= 1) {
       setNavbarTheme("dark");
     } else {
       setNavbarTheme("light");
@@ -28,45 +21,88 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [handleScroll]);
 
-  useEffect(() => {
-    console.log("what");
-    // const navbarCollapse = document.getElementById("navbarCollapse");
-    console.log(document);
-    if (document) {
-      console.log("wtaf");
-      // const bsCollapse =
-      //   navbarCollapse.current &&
-      //   new Collapse(navbarCollapse.current, { toggle: false });
-      // showCollapse ? bsCollapse?.show() : bsCollapse?.hide();
+  const handleToggle = useCallback((expanded) => {
+    if (expanded) {
+      setNavbarTheme("light");
+    } else if (!window.pageYOffset) {
+      setNavbarTheme("dark");
     }
   }, []);
 
   return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-togglable fixed-top navbar-${navbarTheme}`}
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      variant={navbarTheme}
+      fixed="top"
+      onToggle={handleToggle}
     >
       <div className="container">
         <Link href="/">
-          <a className="navbar-brand d-lg-none">Avanti</a>
+          <Navbar.Brand href="/" className="d-lg-none">
+            Avanti
+          </Navbar.Brand>
         </Link>
-
-        <button
-          className="navbar-toggler"
-          type="button"
-          aria-controls="navbarCollapse"
+        <Navbar.Toggle
+          className=""
           aria-expanded="false"
           aria-label="Toggle navigation"
-          onClick={() => setShowCollapse((showCollapse) => !showCollapse)}
         >
           <span className="navbar-toggler-icon"></span>
-        </button>
+        </Navbar.Toggle>
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Item>
+              <Link href="/about">
+                <Nav.Link href="/about" active={router.asPath === "/about"}>
+                  About Us
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/menu">
+                <Nav.Link href="/menu" active={router.asPath === "/menu"}>
+                  Menu
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/events">
+                <Nav.Link href="/events" active={router.asPath === "/events"}>
+                  Special Events
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+          </Nav>
 
-        <div
-          className={`collapse navbar-collapse`}
-          ref={navbarCollapse}
-          id="navbarCollapse"
-        >
-          <ul className="navbar-nav">
+          <Link href="/">
+            <Navbar.Brand
+              onClick={() => console.log("wtaf")}
+              href="/"
+              className="navbar-brand d-none d-lg-flex mx-lg-auto"
+            >
+              Avanti
+            </Navbar.Brand>
+          </Link>
+
+          <Nav>
+            <Nav.Item>
+              <Link href="/gallery">
+                <Nav.Link href="/gallery" active={router.asPath === "/gallery"}>
+                  Some Photos
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href="/contact">
+                <Nav.Link href="/contact" active={router.asPath === "/contact"}>
+                  Contact Us
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+          </Nav>
+
+          {/* <ul className="navbar-nav">
             <li className="nav-item">
               <Link href="/about">
                 <a className={getNavClass("/about")}>About Us</a>
@@ -82,13 +118,13 @@ export default function Navbar() {
                 <a className={getNavClass("/events")}>Special Events</a>
               </Link>
             </li>
-          </ul>
+          </ul> */}
 
-          <Link href="/">
+          {/* <Link href="/">
             <a className="navbar-brand d-none d-lg-flex mx-lg-auto">Avanti</a>
-          </Link>
+          </Link> */}
 
-          <ul className="navbar-nav">
+          {/* <ul className="navbar-nav">
             <li className="nav-item">
               <Link href="/gallery">
                 <a className={getNavClass("/gallery")}>Some Photos</a>
@@ -99,9 +135,73 @@ export default function Navbar() {
                 <a className={getNavClass("/contact")}>Contact Us</a>
               </Link>
             </li>
-          </ul>
-        </div>
+          </ul> */}
+        </Navbar.Collapse>
       </div>
-    </nav>
+    </Navbar>
   );
+}
+
+{
+  /* <nav
+className={`navbar navbar-expand-lg navbar-togglable fixed-top navbar-${navbarTheme}`}
+>
+<div className="container">
+  <Link href="/">
+    <a className="navbar-brand d-lg-none">Avanti</a>
+  </Link>
+
+  <button
+    className="navbar-toggler"
+    type="button"
+    aria-controls="navbarCollapse"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+    onClick={() => setShowCollapse((showCollapse) => !showCollapse)}
+  >
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div
+    className={`collapse navbar-collapse`}
+    ref={navbarCollapse}
+    id="navbarCollapse"
+  >
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <Link href="/about">
+          <a className={getNavClass("/about")}>About Us</a>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link href="/menu">
+          <a className={getNavClass("/menu")}>Menu</a>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link href="/events">
+          <a className={getNavClass("/events")}>Special Events</a>
+        </Link>
+      </li>
+    </ul>
+
+    <Link href="/">
+      <a className="navbar-brand d-none d-lg-flex mx-lg-auto">Avanti</a>
+    </Link>
+
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <Link href="/gallery">
+          <a className={getNavClass("/gallery")}>Some Photos</a>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link href="/contact">
+          <a className={getNavClass("/contact")}>Contact Us</a>
+        </Link>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav> */
 }
