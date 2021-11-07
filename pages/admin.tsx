fe214188a985673b83c05faed4608a8b6ee5c575
samgomena <samgomena@protonmail.com>
2021-11-06 17:24:26 -0700

    fix: Remove a whole bunch of junk; redirect to overview for now

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
-import useUser from "../lib/useUser";
+import useUser from "../lib/hooks/useUser";
 
 const Admin: React.FC = () => {
   const router = useRouter();
   // @ts-ignore
   // const { user } = useUser({ redirectTo: "/login" });
 
-  // Default to overview is no route selected
+  // Default to overview if no route selected
   router.push("/admin/overview");
 
   return null;
