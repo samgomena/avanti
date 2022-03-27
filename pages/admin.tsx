1aa7a18de15237ea41bbb780a30e8ac243de52e9
samgomena <samgomena@protonmail.com>
2022-03-27 14:58:58 -0700

    fix: Setup basic feature flag; only redirect after hydration

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,13 +1,19 @@
 import { useRouter } from "next/router";
 import useUser from "../lib/hooks/useUser";
+import useFlags from "../lib/hooks/useFlags";
+import { useEffect } from "react";
 
 const Admin: React.FC = () => {
-  const router = useRouter();
+  const { adminPage } = useFlags();
+
   // @ts-ignore
   // const { user } = useUser({ redirectTo: "/login" });
 
-  // Default to overview if no route selected
-  router.push("/admin/overview");
+  const router = useRouter();
+  useEffect(() => {
+    // Default to overview if no route selected
+    router.push(adminPage ? "/admin/overview" : "/");
+  }, [adminPage, router]);
 
   return null;
 };
