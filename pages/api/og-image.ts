61ccf40ec59ea4676abe254a28bd1b83a08a165b
samgomena <samgomena@protonmail.com>
2022-04-24 21:06:25 -0700

    fix: Set content-type header on og-image

--- pages/api/og-image.ts
+++ pages/api/og-image.ts
@@ -3,9 +3,14 @@ import { readFileSync } from "fs";
 import path from "path";
 
 const OgImage = async (req: NextApiRequest, res: NextApiResponse) => {
-  const filePath = path.join("public", "assets", "photos", "og-image.png");
-  const image = readFileSync(filePath);
-  res.status(200).send(image);
+  try {
+    const filePath = path.join("public", "assets", "photos", "og-image.png");
+    const image = readFileSync(filePath);
+    res.setHeader("Content-Type", "image/png");
+    res.status(200).send(image);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
 
 export default OgImage;
