4f04e3ac56cbf640b75e5d4da0b775ea88b18b40
samgomena <samgomena@protonmail.com>
2022-04-24 21:29:58 -0700

    wip: Wtaf

--- pages/api/og-image.ts
+++ pages/api/og-image.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { readFileSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
 import path from "path";
 
 const OgImage = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
+    console.log(readdirSync("."));
     const filePath = path.join("public", "assets", "photos", "og-image.png");
     const image = readFileSync(filePath);
     res.setHeader("Content-Type", "image/png");
