6ce15f8df02ea5bdaa3a94d78b727a9f44c519eb
samgomena <samgomena@protonmail.com>
2022-05-24 21:41:41 -0700

    fix: Attempt to fix weird hydration error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import { AppProps } from "next/app";
+import { Suspense } from "react";
+import SSRProvider from "react-bootstrap/SSRProvider";
 import Layout from "../components/Layout";
 import "../styles/scss/theme.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Suspense>
+      <SSRProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SSRProvider>
+    </Suspense>
   );
 }
