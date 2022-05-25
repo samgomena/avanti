7c8a68b9a9644105d9ac138cfd9c9048eed23088
samgomena <samgomena@protonmail.com>
2022-05-25 14:08:27 -0700

    feat: Add tests for heading component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,14 +1,14 @@
 type Props = {
   heading: string;
-  subHeading: string;
+  subHeading?: string;
 };
 
-const Heading: React.FC<Props> = ({ heading, subHeading }) => {
+const Heading: React.FC<Props> = ({ heading, subHeading = "" }) => {
   return (
     <div className="row justify-content-center">
       <div className="col-12 col-md-8 col-lg-6 text-center">
         <h2 className="mb-2">{heading}</h2>
-        <p className="mb-6">{subHeading}</p>
+        {subHeading !== "" && <p className="mb-6">{subHeading}</p>}
       </div>
     </div>
   );
