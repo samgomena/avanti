c54bbad7f5929a9cd95ab9a7117d7ccbd87a23ea
samgomena <samgomena@protonmail.com>
2022-12-12 17:31:58 -0800

    feat: Add some tests that I swore I had already added

--- __tests__/menu.test.tsx
+++ __tests__/menu.test.tsx
@@ -14,4 +14,31 @@ describe("Menu", () => {
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveClass("display-6", "fw-bold", "text-white");
   });
+
+  it("Has all the tabs", () => {
+    render(
+      <ParallaxProvider>
+        <Menu />
+      </ParallaxProvider>
+    );
+
+    ["Dinner", "Drinks"].forEach((_tab) => {
+      const tab = screen.getByText(_tab);
+      expect(tab).toBeInTheDocument();
+      expect(tab).toHaveClass("nav-link");
+      expect(tab);
+    });
+  });
+
+  it("Has dinner selected by default", () => {
+    render(
+      <ParallaxProvider>
+        <Menu />
+      </ParallaxProvider>
+    );
+
+    const tab = screen.getByText("Dinner");
+    expect(tab).toBeInTheDocument();
+    expect(tab).toHaveClass("active");
+  });
 });
