f943082ddb77f2ad079ca11e47b899c4e91a5273
Sam Gomena <samgomena@protonmail.com>
2023-02-10 21:30:18 -0800

    feat: Add alert and show latest alerts

--- components/Alerts.tsx
+++ components/Alerts.tsx
@@ -3,13 +3,20 @@ import Alert from "react-bootstrap/Alert";
 import useAlerts from "../lib/hooks/useAlerts";
 
 const Alerts: React.FC = () => {
-  const [alert] = useAlerts();
+  const alerts = useAlerts();
+  const alert = alerts.at(-1);
   const [show, setShow] = useState(true);
   const now = new Date();
 
+  if (!alert?.start || !alert?.end) {
+    throw Error(
+      `Alert start and/or end is invalid (start=${alert?.start}, end=${alert?.end})`
+    );
+  }
+
   // If alert hasn't started yet or alert has expired then don't show it
   // Note: This is *exclusive* so the alert will show on the days of the start end dates
-  if (now < new Date(alert.start) || now > new Date(alert.end)) {
+  if (now < new Date(alert?.start) || now > new Date(alert?.end)) {
     return null;
   }
 
