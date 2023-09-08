1079c8d0a595ca1f4412367134cf01b9f72a9362
Sam Gomena <samgomena@protonmail.com>
2023-09-07 20:04:18 -0700

    wip: Prototype and todo carousel for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { ParallaxBanner } from "react-scroll-parallax";
 import Alerts from "../components/Alerts";
 import { useFlag } from "../lib/hooks/useFlags";
 import useInfo from "../lib/hooks/useInfo";
+// import Carousel from "react-bootstrap/Carousel";
+// import Image from "next/image";
 
 const layers = [
   {
@@ -18,89 +20,118 @@ export default function Home() {
   const { enabled: reservationsEnabled } = useFlag("reservations");
 
   return (
-    <ParallaxBanner layers={layers} className="h-100">
-      <div className="d-flex flex-column min-vh-100 bg-black-50 pt-10 pt-md-8 pb-7 pb-md-0 position-relative">
-        <div className="container my-auto">
-          <div className="row justify-content-center">
-            <div className="col-12 col-md-8 col-lg-6 text-center">
-              <Alerts />
-              <h1 className="display-1 text-white mb-4">Avanti</h1>
+    <>
+      <ParallaxBanner layers={layers} className="h-100">
+        <div className="d-flex flex-column min-vh-100 bg-black-50 pt-10 pt-md-8 pb-7 pb-md-0 position-relative">
+          <div className="container my-auto">
+            <div className="row justify-content-center">
+              <div className="col-12 col-md-8 col-lg-6 text-center">
+                <Alerts />
+                <h1 className="display-1 text-white mb-4">Avanti</h1>
 
-              <h2 className="text-xs text-white-75">
-                <span className="text-primary">Restaurant</span> / Bar
-              </h2>
+                <h2 className="text-xs text-white-75">
+                  <span className="text-primary">Restaurant</span> / Bar
+                </h2>
 
-              <p className="text-center text-white-75 mb-7">
-                Located in West Linn, Oregon
-              </p>
+                <p className="text-center text-white-75 mb-7">
+                  Located in West Linn, Oregon
+                </p>
 
-              <Link href="/menu">
-                <a className="btn btn-outline-primary text-white text-primary-hover mb-7 mb-md-0 me-2 umami--click--Menu-Button-Clicked">
-                  Menu
-                </a>
-              </Link>
-
-              {reservationsEnabled && (
-                <Link href="https://www.opentable.com/r/avanti-reservations-west-linn?restref=1277137&lang=en-US&ot_source=Restaurant%20website">
-                  <a
-                    className="btn btn-primary text-white text-primary-hover mb-7 mb-md-0 umami--click--OpenTable-Reserve-Clicked"
-                    rel="noopener noreferer"
-                    target="_blank"
-                  >
-                    Reserve a Table
+                <Link href="/menu">
+                  <a className="btn btn-outline-primary text-white text-primary-hover mb-7 mb-md-0 me-2 umami--click--Menu-Button-Clicked">
+                    Menu
                   </a>
                 </Link>
-              )}
+
+                {reservationsEnabled && (
+                  <Link href="https://www.opentable.com/r/avanti-reservations-west-linn?restref=1277137&lang=en-US&ot_source=Restaurant%20website">
+                    <a
+                      className="btn btn-primary text-white text-primary-hover mb-7 mb-md-0 umami--click--OpenTable-Reserve-Clicked"
+                      rel="noopener noreferer"
+                      target="_blank"
+                    >
+                      Reserve a Table
+                    </a>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-md">
+                <ul className="list-inline text-center text-md-start mb-3 my-md-5">
+                  <li className="list-inline-item">
+                    <a
+                      href={info.contact.facebook}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-white-75 text-primary-hover"
+                    >
+                      <span className="visually-hidden">Facebook</span>
+                      <Facebook size={iconSize} />
+                    </a>
+                  </li>
+                  <li className="list-inline-item">
+                    <a
+                      href={info.contact.instagram}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="text-white-75 text-primary-hover"
+                    >
+                      <span className="visually-hidden">Instagram</span>
+                      <Instagram size={iconSize} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-md">
+                <p className="font-serif text-white-75 text-center text-md-end text-lg-end my-md-5">
+                  <i className="fas fa-map-marker-alt text-primary me-3"></i>
+                  <a
+                    href="https://goo.gl/maps/rgDVKbs6SsXyUxiU9"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-white-75 text-primary-hover"
+                  >
+                    <MapPin className="me-2 mb-2" size={18} />
+                    {info.contact.address}
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="container">
-          <div className="row">
-            <div className="col-md">
-              <ul className="list-inline text-center text-md-start mb-3 my-md-5">
-                <li className="list-inline-item">
-                  <a
-                    href={info.contact.facebook}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-white-75 text-primary-hover"
-                  >
-                    <span className="visually-hidden">Facebook</span>
-                    <Facebook size={iconSize} />
-                  </a>
-                </li>
-                <li className="list-inline-item">
-                  <a
-                    href={info.contact.instagram}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="text-white-75 text-primary-hover"
-                  >
-                    <span className="visually-hidden">Instagram</span>
-                    <Instagram size={iconSize} />
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-md">
-              <p className="font-serif text-white-75 text-center text-md-end text-lg-end my-md-5">
-                <i className="fas fa-map-marker-alt text-primary me-3"></i>
-                <a
-                  href="https://goo.gl/maps/rgDVKbs6SsXyUxiU9"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-white-75 text-primary-hover"
-                >
-                  <MapPin className="me-2 mb-2" size={18} />
-                  {info.contact.address}
-                </a>
+      </ParallaxBanner>
+      {/* <section className="bg-light" style={{ height: "30rem" }}>
+        <Carousel fade>
+          <Carousel.Item>
+            <Image src="https://placehold.co/600x400" alt="" layout="fill" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src="https://placehold.co/600x400" alt="" layout="fill" />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src="https://placehold.co/600x400" alt="" layout="fill" />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
               </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </ParallaxBanner>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </section> */}
+    </>
   );
 }
