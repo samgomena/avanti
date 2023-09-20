a6bb107d3b0a6028f389c8ac007c25cffedbac6c
Sam Gomena <samgomena@protonmail.com>
2023-09-19 23:15:58 -0700

    feat: Overhaul admin page

--- pages/admin/overview.tsx
+++ pages/admin/overview.tsx
@@ -1,69 +1,112 @@
-import withAdminNav from "../../lib/withAdminNav";
-import { useMenuBuckets } from "../../lib/hooks/useMenu";
-import {
-  Item,
-  MenuBuckets,
-  Price,
-  Service,
-  Services,
-} from "../../lib/types/menu";
-import { useEffect } from "react";
-import { useFlag } from "../../lib/hooks/useFlags";
-import { useRouter } from "next/router";
-import { GetServerSideProps } from "next/types";
+import { Courses } from "@prisma/client";
 import { getSession } from "next-auth/react";
+import { GetServerSideProps } from "next/types";
+import { useCallback, useState } from "react";
+import { Form, Table } from "react-bootstrap";
+import withAdminNav from "../../lib/withAdminNav";
 
-type MenuBucketStats = { [k in Service]: { total: number; avgPrice: number } };
+type OverviewProps = {
+  course: Courses;
+  disabled: boolean;
+  price: {
+    dinner: number | null;
+    lunch: number | null;
+    hh: number | null;
+    drinks: number | null;
+    dessert: number | null;
+  };
+}[];
 
-const sumPrice = (price: Price) =>
-  Object.values(price).reduce((acc, curr) => acc + curr, 0);
+const Overview: React.FC<{ menu: OverviewProps }> = ({ menu }) => {
+  const [includeDisabled, setIncludeDisabled] = useState(false);
 
-// This is used to simplify the average price calculation in the function below.
-// I don't feel like typing the whole thing so ts-ignore it is :sigh:
-// @ts-ignore
-const sum = (acc, { price }, _, { length }) => acc + sumPrice(price) / length;
-
-const getStats = (menuBuckets: MenuBuckets) =>
-  (["lunch", "dinner", "hh", "drinks"] as Services).reduce(
-    (acc, service) => ({
-      ...acc,
-      [service]: {
-        total: menuBuckets[service].length,
-        avgPrice: menuBuckets[service].reduce(sum, 0).toFixed(2),
-      },
-    }),
-    {} as MenuBucketStats
+  let sumCourse = useCallback(
+    (course: Courses) =>
+      menu
+        .filter((item) => item.course === course)
+        // Either remove disabled entries or include everything
+        .filter((item) => (includeDisabled ? true : item.disabled === false))
+        // Reduce price object into array of values; in the future it could be cool to show values per service period
+        .reduce(
+          (acc, curr) => [
+            ...acc,
+            curr.price.dinner,
+            curr.price.lunch,
+            curr.price.hh,
+            curr.price.drinks,
+            curr.price.dessert,
+          ],
+          [] as (number | null)[]
+        )
+        // This is a shitty workaround to not have to jump through a bunch of hoops with ts + array.filter
+        // See: https://stackoverflow.com/a/59726888/4668680
+        .flatMap((item) => (item ? [item] : []))
+        // Finally, calculate stats!
+        .reduce(
+          (acc, curr, _, { length }) => ({
+            max: curr! >= acc.max ? curr : acc.max,
+            min: curr! <= acc.min ? curr : acc.min,
+            avg: acc.avg + curr! / length,
+          }),
+          { max: 0, min: 1_000, avg: 0.0 }
+        ),
+    [menu, includeDisabled]
   );
 
-const Overview: React.FC = () => {
-  const router = useRouter();
-  const { enabled } = useFlag("adminPage");
+  const appetizerSums = sumCourse("appetizer");
+  const entreeSums = sumCourse("entree");
+  const drinkSums = sumCourse("drink");
+  const dessertSum = sumCourse("dessert");
 
-  !enabled && router.push("/");
-
-  const menuBuckets = useMenuBuckets();
-  const stats = getStats(menuBuckets as MenuBuckets<Item>);
+  const data = [
+    { title: "Appetizers", data: appetizerSums },
+    { title: "Entrees", data: entreeSums },
+    { title: "Drinks", data: drinkSums },
+    { title: "Dessert", data: dessertSum },
+  ];
 
   return (
     <div className="row justify-content-center">
       <div className="col">
         <h3>Overview</h3>
-        <div>
-          <p>Total lunch items: {stats.lunch.total}</p>
-          <p>Average lunch item price: ${stats.lunch.avgPrice}</p>
-        </div>
-        <div>
-          <p>Total dinner items: {stats.dinner.total}</p>
-          <p>Average dinner item price: ${stats.dinner.avgPrice}</p>
-        </div>
-        <div>
-          <p>Total happy hour items: {stats.hh.total}</p>
-          <p>Average happy hour item price: ${stats.hh.avgPrice}</p>
-        </div>
-        <div>
-          <p>Total drinks: {stats.drinks.total}</p>
-          <p>Average drink price: ${stats.drinks.avgPrice}</p>
-        </div>
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th></th>
+              {data.map((item) => (
+                <th key={item.title}>{item.title}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Average</td>
+              {data.map((item, idx) => (
+                <td key={`avg_${idx}`}>${item.data.avg.toFixed(2)}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>Min</td>
+              {data.map((item, idx) => (
+                <td key={`avg_${idx}`}>${item.data.min}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>Max</td>
+              {data.map((item, idx) => (
+                <td key={`avg_${idx}`}>${item.data.max}</td>
+              ))}
+            </tr>
+          </tbody>
+        </Table>
+        <Form.Check
+          type="checkbox"
+          checked={includeDisabled}
+          onChange={(event) => setIncludeDisabled(event.target.checked)}
+          label="Include hidden"
+          id="include-hidden-checkbox"
+        />
       </div>
     </div>
   );
@@ -81,7 +124,26 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
       },
     };
   }
+
+  const menu = await prisma?.menu.findMany({
+    select: {
+      course: true,
+      disabled: true,
+      price: {
+        select: {
+          dinner: true,
+          lunch: true,
+          hh: true,
+          drinks: true,
+          dessert: true,
+        },
+      },
+    },
+  });
+
   return {
-    props: {},
+    props: {
+      menu,
+    },
   };
 };
