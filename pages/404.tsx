f80f188f540660b930c412212d394b94c05a381d
Sam Gomena <samgomena@protonmail.com>
2023-10-28 00:58:14 -0700

    fix: Add photo to 404 page (also update heading)

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,16 +2,21 @@ import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import Header from "../components/Header";
 import Section from "../components/Section";
+import Heading from "../components/Heading";
 
 const FourOhFour: React.FC = () => {
   return (
     <>
-      <Header title="There's nothing here!" image="" />
+      <Header
+        title="Whoops! There's nothing here..."
+        image="avanti_restaurant_2.jpg"
+      />
       <Section>
+        <Heading heading="Terribly sorry about that." />
         <div className="row justify-content-center">
           <div className="col d-flex justify-content-center">
-            <Button variant="outline-primary">
-              <Link href="/">Go back home</Link>
+            <Button variant="outline-primary" className="mt-4">
+              <Link href="/">Go back home?</Link>
             </Button>
           </div>
         </div>
