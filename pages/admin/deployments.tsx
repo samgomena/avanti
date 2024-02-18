af996db9f88be8e864f0cff89ed87829876e7056
Sam Gomena <samgomena@protonmail.com>
2024-02-17 21:58:50 -0800

    fix: Gate deployments behind login page

--- pages/admin/deployments.tsx
+++ pages/admin/deployments.tsx
@@ -1,7 +1,25 @@
 import withAdminNav from "@/lib/withAdminNav";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 
 const Deployments: React.FC = () => {
   return <div>Deployments</div>;
 };
 
 export default withAdminNav(Deployments);
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: `/login?wantsUrl=${ctx.resolvedUrl}`,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
