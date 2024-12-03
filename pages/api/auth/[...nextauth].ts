726457250e3bb85ab2fb63ebeac6a2dabb20aa5a
Sam Gomena <samgomena@protonmail.com>
2024-12-02 23:21:23 -0800

    fix: Type error with port in nextauth email provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,7 +12,7 @@ export const authConfig: NextAuthOptions = {
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: Number(process.env.EMAIL_SERVER_PORT),
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD,
