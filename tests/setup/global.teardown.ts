5c9e9d0c61100ab4d2b1a8533db112460b7bbe06
Sam Gomena <samgomena@protonmail.com>
2024-12-16 22:46:08 -0800

    fix: Make sure we're connected to the test database in tests. again.

--- tests/setup/global.teardown.ts
+++ tests/setup/global.teardown.ts
@@ -3,14 +3,6 @@ import { test as teardown } from "@playwright/test";
 import { testUserEmail } from "./constants";
 
 teardown("delete database", async () => {
-  const connectionString = process.env.DATABASE_URL;
-  if (
-    !connectionString?.includes("/avanti:dev") &&
-    !connectionString?.includes("localhost:5432/test")
-  ) {
-    throw new Error("Not connected to test database. Aborting...");
-  }
-
   console.log("deleting test database...");
   await db.user.delete({
     where: {
